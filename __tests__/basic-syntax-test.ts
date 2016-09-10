becb4f9ea6fa209c63bc99aaa0c21923911f71c2
Kan Ouivirach <kan@prontomarketing.com>
2016-09-10 10:20:14 +0700

    Make type case pass

--- __tests__/basic-syntax-test.ts
+++ __tests__/basic-syntax-test.ts
@@ -20,21 +20,21 @@ describe('TypeScript', function() {
     it('cannot reassign twice', function() {
       const CONSTANT = 1;
 
-      // If we uncomment the line below, we'll get the compile error
-      //CONSTANT = 2;
+      // if we uncomment the line below, we'll get the compile error
+      // CONSTANT = 2;
 
       expect(CONSTANT).toEqual(1);
     });
   });
 
-  xdescribe('type', function() {
+  describe('type', function() {
     it('is number', function() {
-      let x: number;
+      let x: number = 0;
       expect(x).toEqual(0);
     });
 
     it('is string', function() {
-      let word: string;
+      let word: string = 'Hello TypeScript';
       expect(word).toEqual('Hello TypeScript');
     });
 
@@ -42,39 +42,51 @@ describe('TypeScript', function() {
       it('use for make string multiple line', function() {
         let word: string = `Line 1
 Line 2`;
-        expect(word).toEqual('');
+        expect(word).toEqual('Line 1\nLine 2');
       });
 
       it('use for embed variable to string', function() {
         let luckyNumber: number = 7;
-        let word: string = `lucky number is `;
+        let word: string = `lucky number is ${luckyNumber}`;
         expect(word).toEqual('lucky number is 7');
       });
     });
 
     it('is boolean', function() {
-      let tf: boolean;
+      let tf: boolean = true;
       expect(tf).toEqual(true);
     });
 
     describe('array', function() {
       it('use type Array<type> for assign array', function() {
-        let numbers = [1, 2, 3];
+        let numbers: Array<number> = [1, 2, 3];
+        expect(numbers).toEqual([1, 2, 3]);
+      });
+
+      it('use more than one type (union type): Array<type | another type> for assign array', function() {
+        let numbers: Array<number | string> = ['hello', 1, 2, 3];
+        expect(numbers).toEqual(['hello', 1, 2, 3]);
       });
 
       it('can use <type>[] instead of Array', function() {
-        let numbers = [1, 2, 3];
+        let numbers: number[] = [1, 2, 3];
+        expect(numbers).toEqual([1, 2, 3]);
       });
     });
 
     describe('any', function() {
       it('can assign any type to variable', function() {
         let x: any = 1;
+
+        // here we can reassign a different type to x
+        x = 'text';
+
         expect(x).toEqual('text');
       });
 
       it('implicit type any by default', function() {
-        let x = 1;
+        // but here we cannot reassign a different type to x, so we need to change the value to be of the type we want
+        let x = 'text';
         expect(x).toEqual('text');
       });
     });
