e8b86091b6debd5aeaa2a44013efe62046fb3084
Kan Ouivirach <kan@prontomarketing.com>
2016-09-10 13:30:12 +0700

    Separate actual code from tests

--- __tests__/captcha-generator-test.ts
+++ __tests__/captcha-generator-test.ts
@@ -1,43 +1,6 @@
 /// <reference path="../typings/jest/jest.d.ts" />
 
-class Captcha {
-  convert_number_to_number_text(num: number): string {
-    let number_text = {
-      1: "One",
-      2: "Two",
-      3: "Three",
-      4: "Four",
-      5: "Five",
-      6: "Six",
-      7: "Seven",
-      8: "Eight",
-      9: "Nine",
-    }
-    return number_text[num];
-  }
-
-  convert_operator_to_operator_text(num: number): string {
-    let operator_text = {
-      1: "+",
-      2: "-",
-      3: "*",
-    }
-    return operator_text[num];
-  }
-
-  generate(pattern: number, left: number, right: number, operator: number): string {
-    let converted_right = this.convert_number_to_number_text(right);
-    let converted_operator = this.convert_operator_to_operator_text(operator);
-
-    if (pattern === 1) {
-      return `${left} ${converted_operator} ${converted_right}`;
-    }
-    else if (pattern === 2) {
-      return `${converted_right} ${converted_operator} ${left}`;
-    }
-  };
-};
-
+import {Captcha} from "../src/captcha";
 
 describe("Captcha", function() {
   var captcha;
